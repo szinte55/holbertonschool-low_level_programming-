b36d8db4365c3abbae84126799286c1bddfdfe44
szinte55 <szinte55@gmail.com>
2019-02-09 01:31:13 +0000

    commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	char h;
 
-	for (h = 'a'; h <= 'z'; x++)
+	for (h = 'a'; h <= 'z'; h++)
 		putchar(h);
-	for (h = 'A'; h <= 'Z'; x++)
+	for (h = 'A'; h <= 'Z'; h++)
 		putchar(h);
 	putchar('\n');
 	return (0);
