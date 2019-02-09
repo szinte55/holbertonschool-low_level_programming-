bb0196cb5d64c246b62b923c0545f6ec42302793
szinte55 <szinte55@gmail.com>
2019-02-09 00:59:29 +0000

    commits

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,11 +5,12 @@
  */
 int main(void)
 {
-        char i;
-        for (i = 'a' ; i <= 'z' ; i++)
-        {
-                putchar (i);
-        }
-        putchar ('\n');
-        return (0);
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		putchar (i);
+	}
+	putchar ('\n');
+	return (0);
 }
