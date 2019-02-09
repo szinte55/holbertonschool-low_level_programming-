ce284fae9967cd5fdc69d383083a404f520d38aa
szinte55 <szinte55@gmail.com>
2019-02-09 01:21:01 +0000

    first commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Out put alphabet in lower case then upper case
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char h;
+
+	for (h = 'a'; h <= 'z'; x++)
+		putchar(h);
+	for (h = 'A'; h <= 'Z'; x++)
+		putchar(h);
+	putchar('\n');
+	return (0);
+}
