fb12bb5970cb6fe9570ef7479000f22312513b39
szinte55 <szinte55@gmail.com>
2019-02-13 00:40:54 +0000

    "commit"

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,13 +2,16 @@
 /**
  * print_last_digit - print last digit of a number using putchar
  * @i: integer being evaluated
+ *
  * Return: the last digit of a number
  */
 
 int print_last_digit(int i)
 {
-	if (i >= 0)
-		return (_putchar(i % 10 + '0'));
-	else
-		return (_putchar((-1*i) % 10 + '0'));
+	int j = i % 10;
+
+	if (j < 0)
+		j = -j;
+	_putchar(j + '0');
+	return (j);
 }
