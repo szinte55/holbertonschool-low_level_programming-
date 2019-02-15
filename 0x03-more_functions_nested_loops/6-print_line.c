40396bd2aeb678c01ec6f3618d48ea9c0dacd558
szinte55 <szinte55@gmail.com>
2019-02-15 21:52:00 +0000

    "commit"

---
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - Print the numbers 0 to 14 ten times followed by new line.
+ *
+ * Return: Void
+ */
+void more_numbers(void)
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
