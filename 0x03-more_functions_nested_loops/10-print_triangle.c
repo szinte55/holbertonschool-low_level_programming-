e095b008d9b1bd607179acbc55b1f681eac16a1e
szinte55 <szinte55@gmail.com>
2019-02-16 01:24:35 +0000

    "commit"

---
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_triangle - Print a triangle
+ *
+ * @size: length of base of triangle
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size >= 0)
+	{
+		for (i = 1; size >= i; i++)
+		{
+			for (j = 1; size - i >= j; j++)
+				_putchar(' ');
+			for (j = 1; j <= i; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	if (size <= 0)
+		_putchar('\n');
+}
