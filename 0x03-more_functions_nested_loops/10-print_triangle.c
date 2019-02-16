dc2520a8101fed46af244dd5c68a5900470f7d4c
szinte55 <szinte55@gmail.com>
2019-02-16 05:32:51 +0000

    "commit"

--- 0x03-more_functions_nested_loops/10-print_triangle.c
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -6,18 +6,18 @@
  */
 void print_triangle(int size)
 {
-	int i, j;
+	int i, j, k;
 
 	if (size > 0)
 	{
-		for (i = 1; size >= i; i++)
+		for (i = 0; i < size; i++)
 		{
-			for (j = 1; size - i >= j; j++)
+			for (j = 0; j <= size -(i+1); j++)
 				_putchar(' ');
-			for (j = 1; j <= i; j++)
+			for (k = 0; k <= size - j; k++)
 				_putchar('#');
 			_putchar('\n');
-		}
+		}             
 	}
 	if (size <= 0)
 		_putchar('\n');
