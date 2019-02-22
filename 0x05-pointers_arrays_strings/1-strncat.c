d723293af5498beecadfbd5bf559cc5138d35ae7
szinte55 <szinte55@gmail.com>
2019-02-22 23:15:51 +0000

    "commit"

--- 0x05-pointers_arrays_strings/1-strncat.c
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -14,10 +14,9 @@ char *_strncat(char *dest, char *src, int n)
 	for (i = 0 ; dest[i] != 0; i++)
 		;
 
-	for (j = 0; src[j] != '\0' && n > 0)
+	for (j = 0; src[j] != '\0' && n > 0; j++)
 	{
 		dest[i] = src[j];
-		j++;
 		i++;
 		n--;
 	}
